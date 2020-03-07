a011394002d77a940b3e4705c986e56a7ca4a276
Diane Kaplan <dianekaplan@gmail.com>
2020-03-07 14:31:01 -0500

    added family bools to admin index pages

--- mysite/familytree/admin.py
+++ mysite/familytree/admin.py
@@ -8,13 +8,13 @@ from .models import Person, Family
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     search_fields = ('display_name', 'first', 'last')
-    list_display = ('display_name', 'birthdate_note', 'origin_family', 'gedcom_UUID', 'created_at', 'reviewed')
+    list_display = ('display_name', 'birthdate_note', 'origin_family', 'gedcom_UUID', 'keem_line', 'husband_line', 'kemler_line', 'kaplan_line','created_at', 'reviewed')
     ordering = ('-created_at', 'display_name')
     pass
 
 @admin.register(Family)
 class FamilyAdmin(admin.ModelAdmin):
     search_fields = ('display_name',)
-    list_display = ('display_name', 'created_at', 'updated_at', 'reviewed')
+    list_display = ('display_name', 'keem_line', 'husband_line', 'kemler_line', 'kaplan_line','created_at', 'reviewed')
     ordering = ('-created_at', 'display_name')
     pass
