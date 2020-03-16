0567054cbd802d37ab105b6a0f1656daf7623125
Diane Kaplan <dianekaplan@gmail.com>
2020-03-16 16:20:11 -0400

    updating/troubleshooting top nav person link

--- mysite/familytree/models.py
+++ mysite/familytree/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 class Branch(models.Model):
     display_name = models.CharField(max_length=50, blank=True)
@@ -178,6 +179,7 @@ class Note(models.Model):
     def __str__(self):
         return self.author_name + self.body
 
+
 class Profile(models.Model): # This class holds additional info for user records
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     person = models.ForeignKey('Person', null=True, blank=True, on_delete=models.SET_NULL)
@@ -194,5 +196,4 @@ class Profile(models.Model): # This class holds additional info for user records
 
     def __str__(self):
         return self.person.display_name
-
 
