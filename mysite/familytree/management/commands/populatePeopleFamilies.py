bdc0f685a88de48286ca9047acf58836354c6f9b
Diane Kaplan <dianekaplan@gmail.com>
2020-12-31 08:11:32 -0500

    migration scripts and updated models.py

---
+++ mysite/familytree/management/commands/populatePeopleFamilies.py
@@ -0,0 +1,190 @@
+from django.core.management.base import BaseCommand, CommandError
+from gedcom.element.individual import IndividualElement
+from gedcom.element.family import FamilyElement
+from gedcom.parser import Parser
+from ...models import Person, Family
+from pathlib import Path
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = 'Populates branch values for imported people (internal)'
+    missing_args_message = 'Please specify file to use'
+    gedcom_person_records = 0;
+    gedcom_family_records = 0;
+    person_added_count = 0;
+    family_added_count = 0;
+    person_skipped_count = 0;
+    child_family_dict = {}
+
+    def add_arguments(self, parser):
+        parser.add_argument('file name', type=Path, help='Name of GEDCOM file to import from')
+
+    def handle(self, *args, **kwargs):
+        filename = kwargs['file name']
+
+        # validate that the user gave file with extension ged
+        if filename.suffix!= '.ged':
+            raise CommandError('Please specify GEDCOM file, ex: myGedcom.ged')
+
+        # Check that the file is there
+        path = Path("familytree/management/commands/gedcom_files/") # @@TODO: update to take the whole path (so it doesn't need to be saved in a particular folder)
+        path_plus_file = path.joinpath(filename)
+
+        if (path_plus_file.is_file()):
+            gedcom_parser = Parser()
+            gedcom_parser.parse_file(path_plus_file)
+            root_child_elements = gedcom_parser.get_root_child_elements()
+
+            for element in root_child_elements:
+                # find/add people records
+                if isinstance(element, IndividualElement):
+                    self.handle_person(element)
+
+                # find/add family records (person records exist already, so we can look up parent references)
+                # also save intermediate dictionary: CHIL INDI - family INDI
+                if isinstance(element, FamilyElement):
+                    self.handle_family(element)
+
+            #now that we've saved all the people and families, populate orig_family on people records
+            self.add_orig_family_values(self.child_family_dict)
+
+        else:
+            raise CommandError('That gedcom file does not exist in the expected directory')
+
+        # gather run results
+        run_results = 'gedcom_person_records: ' + str(self.gedcom_person_records) +  '\n'
+        run_results += 'gedcom_family_records: ' + str(self.gedcom_family_records) +  '\n'
+        run_results += 'person_added_count: ' + str(self.person_added_count) +  '\n'
+        run_results += 'person_skipped_count: ' + str(self.person_skipped_count) +  '\n'
+        run_results += 'family_added_count: ' + str(self.family_added_count) +  '\n'
+
+        # Display and log them
+        self.stdout.write(self.style.SUCCESS('You passed filename: ') + str(filename))
+        self.stdout.write(run_results )
+        f = open('ImportInfo.txt', 'w')
+        f.write(run_results)
+        f.closed
+
+    def handle_person(self, element):
+        self.gedcom_person_records += 1
+        (gedcom_first_middle, last) = element.get_name()
+        gedcom_UUID = ''
+        skip_record = False
+
+        if "INDI" in str(element):
+            gedcom_indi = str(element).replace(" INDI", "").replace("0 ", "").replace("\r\n", "")
+        # get the fields available from our parser
+        (birthdate, birthplace, sources) = element.get_birth_data()
+        sex = element.get_gender()
+        occupation = element.get_occupation()
+        (deathdate, deathplace, sources) = element.get_death_data()
+        display_name = gedcom_first_middle + " " + last
+
+        # check the children for our custom UUID field (applicable for subsequent imports)
+        element_children = element.get_child_elements()
+        for child in element_children:
+            # print(element.to_gedcom_string(recursive=True))
+            if "ALIA" in str(child):
+                gedcom_UUID = str(child).replace("1 ALIA ", "").replace("\r\n", "")
+
+                try:
+                    matching_record = Person.objects.get(gedcom_UUID=gedcom_UUID)
+                    if matching_record != "":
+                        skip_record = self.check_matching_record(matching_record, element)
+                except:
+                    print(gedcom_first_middle + " " + last + " gedcom record with ALIA tag had no matching value in our data")
+
+        if not skip_record:
+            # make the person record
+            (obj, created_bool) = Person.objects.get_or_create(gedcom_indi=gedcom_indi, gedcom_UUID=gedcom_UUID,
+                                                           first=gedcom_first_middle, last=last,
+                                                           display_name=display_name, birthdate_note=birthdate,
+                                                           birthplace=birthplace, sex=sex, work=occupation,
+                                                           deathdate_note=deathdate, resting_place=deathplace,
+                                                               show_on_landing_page=True,
+                                                           created_at = timezone.now(), updated_at = timezone.now(), reviewed=False)
+            if created_bool:
+                self.person_added_count += 1
+        else:
+            self.person_skipped_count += 1
+
+    def handle_family(self, element):
+        gedcom_indi = str(element).replace(" FAM", "").replace("0 ", "").replace("\r\n", "")
+        self.gedcom_family_records += 1
+        no_kids_bool = True
+        wife = ""
+        husband = ""
+        element_children = element.get_child_elements()
+        marriage_date = ""
+        husband_indi= ""
+        wife_indi = ""
+
+        for child in element_children:  # @TODO: look back at gedcom_parser.get_family_members approach: there you see FAMS but not wife vs husband
+            # print(element.to_gedcom_string(recursive=True))
+            if "MARR" in str(child):
+                marriage_info = child.get_child_elements()
+                for item in marriage_info:
+                    if "PLAC" in str(item):
+                        marriage_place = str(item).replace("2 PLAC ", "")
+                    if "DATE" in str(item):
+                        marriage_date = str(item).replace("2 DATE ", "")
+            if "WIFE" in str(child):
+                wife_indi = str(child).replace("1 WIFE ", "").replace("\r\n", "")
+                try:
+                    this_person = Person.objects.get(gedcom_indi=wife_indi) #this person does not exist for our family @F118@
+                    wife = this_person
+                except:
+                    print("For family " + gedcom_indi + ", couldn't find person matching wife_indi " + wife_indi)
+            if "HUSB" in str(child):
+                husband_indi = str(child).replace("1 HUSB ", "").replace("\r\n", "")
+                try:
+                    this_person = Person.objects.get(gedcom_indi=husband_indi)
+                    husband = this_person
+                except:
+                    print("For family " + gedcom_indi + ", couldn't find person matching husband_indi " + husband_indi)
+            if "CHIL" in str(child):
+                no_kids_bool = False
+                child_indi = str(child).replace("1 CHIL ", "").replace("\r\n",
+                                                                       "")  # @FIXME: originally did += for text field, but if this works we won't need to use that text field
+                if child_indi not in self.child_family_dict:
+                    self.child_family_dict[child_indi] = gedcom_indi  # add dictionary entry for the child
+
+        display_name = (wife.display_name + " & " if wife != "" else "(unknown name) & ")
+        display_name += (husband.display_name if husband != "" else "(unknown name)")
+
+        (obj, created_bool) = Family.objects.get_or_create(gedcom_indi=gedcom_indi, display_name=display_name,
+                                                           wife_indi=wife_indi, husband_indi=husband_indi,
+                                                           marriage_date_note=marriage_date, no_kids_bool=no_kids_bool,
+                                                           created_at = timezone.now(), updated_at = timezone.now(), reviewed=False)
+
+        # then link the parents that are known
+        if wife != "":
+            obj.wife = wife
+            obj.save()
+        if husband != "":
+            obj.husband = husband
+            obj.save()
+
+        if created_bool:
+            self.family_added_count += 1
+
+    def add_orig_family_values(self, child_family_dict):
+        # loop through dictionary
+        for entry in self.child_family_dict:
+            try:
+                this_person = Person.objects.get(gedcom_indi=entry)
+                orig_family = Family.objects.get(gedcom_indi=self.child_family_dict.get(entry))
+                this_person.origin_family = orig_family
+                this_person.save()
+            except:
+                print("We have a family person whose record didn't get saved with gedcom_indi: " + entry)
+
+    def check_matching_record(self, matching_record, element):
+        print("This record with ALIA tag exists already: " + matching_record.first + " " +  matching_record.last)
+        # @TODO: come back and look into whether there are fields we'd want to update (ex: add birthdate, etc)
+        # If gedcom entry has values for fields we have blank, can fill them in
+
+        # matching_record.reviewed = False
+        # matching_record.save()
+        skip_record = True
+        return skip_record
--- mysite/familytree/models.py
+++ mysite/familytree/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 
+
 class Branch(models.Model):
     display_name = models.CharField(max_length=50, blank=True)
 
@@ -13,45 +14,46 @@ class Branch(models.Model):
         return self.display_name
 
 class Person(models.Model):
-    gedcom_indi = models.CharField(max_length=10, blank=True, default='')
-    gedcom_UUID = models.CharField(max_length=40, blank=True, default='')
+    gedcom_indi = models.CharField(max_length=10, null=True, blank=True, default='')
+    gedcom_uuid = models.CharField(max_length=40, null=True, blank=True, default='')
     first = models.CharField(max_length=30, blank=True, default='')
-    middle = models.CharField(max_length=20, blank=True, default='')
+    middle = models.CharField(max_length=20, null=True, blank=True, default='')
     last = models.CharField(max_length=20, blank=True, default='')
-    maiden = models.CharField(max_length=20, blank=True, default='')
-    nickname = models.CharField(max_length=20, blank=True, default='')
-    display_name = models.CharField(max_length=40)
+    maiden = models.CharField(max_length=20, null=True, blank=True, default='')
+    nickname = models.CharField(max_length=20, null=True, blank=True, default='')
+    display_name = models.CharField(max_length=40, null=True)
     branches = models.ManyToManyField(Branch, null=True, blank=True)
 
     birthdate = models.DateField(null=True, blank=True)
-    birthdate_note = models.CharField(max_length=55, blank=True, default='')
-    birthplace = models.CharField(max_length=60, blank=True, default='')
-    origin_family = models.ForeignKey('Family', null=True, blank=True, on_delete=models.SET_NULL)
-    orig_fam_indi = models.CharField(max_length=10, blank=True, default='')
+    birthdate_note = models.CharField(max_length=55, null=True, blank=True, default='')
+    birthplace = models.CharField(max_length=60, null=True, blank=True, default='')
+    family_id = models.ForeignKey('Family', null=True, blank=True, on_delete=models.SET_NULL) # person's origin family
+    orig_fam_indi = models.CharField(max_length=10, null=True, blank=True, default='')
     keem_line = models.BooleanField(null=True, default=False)     #@TODO: remove when all set
     husband_line = models.BooleanField(null=True, default=False)  #@TODO: remove when all set
     kemler_line = models.BooleanField(null=True, default=False)   #@TODO: remove when all set
     kaplan_line = models.BooleanField(null=True, default=False)   #@TODO: remove when all set
-    sex = models.CharField(max_length=2, blank=True, default='')
-    origin = models.CharField(max_length=100, blank=True, default='') # big description of background, probably will remove
-    face = models.CharField(max_length=20, blank=True, default='')
-    current_location = models.CharField(max_length=20, blank=True, default='')
-    work = models.CharField(max_length=150, blank=True, default='')
-    interests = models.CharField(max_length=150, blank=True, default='')
-    education = models.CharField(max_length=150, blank=True, default='')
-    resting_place = models.CharField(max_length=60, blank=True, default='')
-    deathdate_note = models.CharField(max_length=175, blank=True, default='')
+    sex = models.CharField(max_length=2, null=True, blank=True, default='')
+    origin = models.CharField(max_length=100, null=True, blank=True, default='') # big description of background, probably will remove
+    face = models.CharField(max_length=25, null=True, blank=True, default='')
+    current_location = models.CharField(max_length=75, null=True, blank=True, default='')
+    work = models.CharField(max_length=300, null=True, blank=True, default='')
+    interests = models.CharField(max_length=300, null=True, blank=True, default='')
+    education = models.CharField(max_length=300, null=True, blank=True, default='')
+    resting_place = models.CharField(max_length=100, null=True, blank=True, default='')
+    deathdate = models.DateField(null=True, blank=True)
+    deathdate_note = models.CharField(max_length=175, null=True, blank=True, default='')
     hidden = models.BooleanField(null=True, default=False)
     adopted = models.BooleanField(null=True, default=False)
     direct_line = models.BooleanField(null=True, default=False)
-    show_on_landing_page = models.BooleanField(null=False, default=False)
+    show_on_landing_page = models.BooleanField(null=True, default=False)
     sibling_seq = models.IntegerField(blank=True, null=True)  # will later update families to handle this individually
-    notes1 = models.CharField(max_length=300, blank=True, default='')
-    notes2 = models.CharField(max_length=300, blank=True, default='')
-    notes3 = models.CharField(max_length=300, blank=True, default='')
-    flag1 = models.CharField(max_length=10, blank=True, default='') # will probably get rid of these anyway
-    flag2 = models.CharField(max_length=10, blank=True, default='') # will probably get rid of these anyway
-    flag3 = models.CharField(max_length=10, blank=True, default='') # will probably get rid of these anyway
+    notes1 = models.CharField(max_length=600, null=True, blank=True, default='')
+    notes2 = models.CharField(max_length=600, null=True, blank=True, default='')
+    notes3 = models.CharField(max_length=600, null=True, blank=True, default='')
+    flag1 = models.CharField(max_length=10, null=True, blank=True, default='') # will probably get rid of these anyway
+    flag2 = models.CharField(max_length=10, null=True, blank=True, default='') # will probably get rid of these anyway
+    flag3 = models.CharField(max_length=10, null=True, blank=True, default='') # will probably get rid of these anyway
     created_at = models.DateTimeField(null=True, blank=True)
     updated_at = models.DateTimeField(null=True, blank=True)
     reviewed = models.BooleanField(null=True, default=False)
@@ -67,16 +69,17 @@ class Person(models.Model):
     def __str__(self):
         return self.first + " " + self.last
 
-
 class Family(models.Model):
-    gedcom_indi = models.CharField(max_length=10, blank=True, default='')
+    gedcom_indi = models.CharField(max_length=10, null=True, blank=True, default='')
     display_name = models.CharField(max_length=50, blank=True)
     wife = models.ForeignKey(Person, null=True, blank=True, on_delete=models.SET_NULL, related_name='partner1')
     husband = models.ForeignKey(Person, null=True, blank=True, on_delete=models.SET_NULL, related_name='partner2')
-    wife_indi = models.CharField(max_length=10, blank=True, default='')
-    husband_indi = models.CharField(max_length=10, blank=True, default='')
-    child_indi = models.CharField(max_length=10, blank=True, default='')  # temporary- still need this?
+    wife_indi = models.CharField(max_length=10, null=True, blank=True, default='')
+    husband_indi = models.CharField(max_length=10, null=True, blank=True, default='')
+    child_indi = models.CharField(max_length=10, null=True, blank=True, default='')  # temporary- still need this?
     no_kids_bool = models.BooleanField(null=True)
+    original_family = models.BooleanField(null=True)
+    original_family_text = models.CharField(max_length=600, null=True, blank=True, default='')
     branches = models.ManyToManyField(Branch, null=True, blank=True)
 
     keem_line = models.BooleanField(null=True, default=False)    #@TODO: remove when all set
@@ -84,16 +87,16 @@ class Family(models.Model):
     kemler_line = models.BooleanField(null=True, default=False)  #@TODO: remove when all set
     kaplan_line = models.BooleanField(null=True, default=False)  #@TODO: remove when all set
     marriage_date = models.DateField(null=True, blank=True)
-    marriage_date_note = models.CharField(max_length=100, blank=True, default='')
+    marriage_date_note = models.CharField(max_length=100, null=True, blank=True, default='')
     divorced = models.BooleanField(null=True, default=False)
     created_at = models.DateTimeField(null=True, blank=True)
     updated_at = models.DateTimeField(null=True, blank=True)
-    show_on_branch_view = models.BooleanField(null=False, default=False)
+    show_on_branch_view = models.BooleanField(null=True, default=False)
     sequence = models.IntegerField(blank=True, null=True)
-    branch = models.IntegerField(blank=True, null=True)
+    notes1 = models.CharField(max_length=600, null=True, blank=True, default='')
+    notes2 = models.CharField(max_length=600, null=True, blank=True, default='')
+    # branch = models.IntegerField(blank=True, null=True)
     branch_seq = models.IntegerField(blank=True, null=True)
-    flag1 = models.CharField(max_length=10, blank=True, default='') # will probably get rid of these anyway
-    flag2 = models.CharField(max_length=10, blank=True, default='') # will probably get rid of these anyway
     reviewed = models.BooleanField(null=True, default=False)
 
     class Meta(object):
@@ -107,13 +110,13 @@ class Family(models.Model):
         return self.display_name
 
 class Image(models.Model):
-    big_name = models.CharField(max_length=50, blank=True) # this field will always be there
-    med_name = models.CharField(max_length=50, blank=True) # optional file name for different medium sized image (rather than just resized)
-    little_name = models.CharField(max_length=50, blank=True) # optional file name for zoomed-in thumbnail (rather than just resized)
-    caption = models.CharField(max_length=50, blank=True)
+    big_name = models.CharField(max_length=50, null=True, blank=True) # this field will always be there
+    med_name = models.CharField(max_length=50, null=True, blank=True) # optional file name for different medium sized image (rather than just resized)
+    little_name = models.CharField(max_length=50, null=True, blank=True) # optional file name for zoomed-in thumbnail (rather than just resized)
+    caption = models.CharField(max_length=50, null=True, blank=True)
     branches = models.ManyToManyField(Branch, null=True, blank=True)
 
-    year = models.CharField(max_length=10, blank=True)
+    year = models.CharField(max_length=20, null=True, blank=True)
     person = models.ForeignKey(Person, null=True, blank=True, on_delete=models.SET_NULL, related_name='person')
     family = models.ForeignKey(Family, null=True, blank=True, on_delete=models.SET_NULL, related_name='family')
     featured = models.IntegerField(null=True, default=False)
@@ -160,10 +163,9 @@ class ImagePerson(models.Model):
         return str(self.image_id)
 
 class Note(models.Model):
-    type = models.IntegerField(null=True)  # 1 for person, 2 for family
     author = models.ForeignKey(Person, null=True, blank=True, on_delete=models.SET_NULL, related_name='author')
-    author_name = models.CharField(max_length=50, blank=True)
-    body = models.CharField(max_length=1000, blank=True)
+    author_name = models.CharField(max_length=50, null=True, blank=True)
+    body = models.CharField(max_length=3000, blank=True)
     date = models.DateField(null=True, blank=True)
     person = models.ForeignKey(Person, null=True, blank=True, on_delete=models.SET_NULL, related_name='note_person')
     family = models.ForeignKey(Family, null=True, blank=True, on_delete=models.SET_NULL, related_name='family_note')
@@ -197,3 +199,32 @@ class Profile(models.Model): # This class holds additional info for user records
     def __str__(self):
         return self.person.display_name
 
+
+class Story(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    image = models.CharField(max_length=255, null=True, blank=True) # note- this is NOT an Image object
+    intro = models.CharField(max_length=2000, null=True, blank=True)
+    slug = models.CharField(max_length=255, null=True, blank=True)
+    source = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta(object):
+        verbose_name_plural = 'Stories'
+        db_table = 'stories'
+
+    def __str__(self):
+        return self.description
+
+class PersonStory(models.Model):
+    story = models.ForeignKey(Story, null=True, blank=True, on_delete=models.SET_NULL)
+    person = models.ForeignKey(Person, null=True, blank=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta(object):
+        verbose_name_plural = 'PersonStory records'
+        db_table = 'person_story'
+
+    def __str__(self):
+        return str(self.story.description)
