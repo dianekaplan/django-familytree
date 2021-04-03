50c490f7fd242d6285a44b3eee9178bfc7b1bb46
Diane Kaplan <dianekaplan@gmail.com>
2021-04-03 07:20:28 -0400

    script to update <100 year olds to living

---
+++ mysite/familytree/management/commands/populateLivingBool.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...models import Person
+from datetime import date
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = 'Populates display names for migrated database (internal use)'
+
+    # update living bool from false to true if: birth year within 100 years and deathdate fields are empty
+
+    def handle(self, *args, **options):
+        people = Person.objects.all()
+        today = date.today()
+
+        print("People we know were born within 100 years (will set to living): ")
+        for person in people:
+            this_display_name = ''
+            if person.nickname:
+                this_display_name = person.nickname.strip() + " " + person.last.strip()
+            else:
+                this_display_name = person.first.strip() + " " + person.last.strip()
+
+            if not(person.deathdate_note) and not(person.deathdate):
+                birthdate_info = ''
+                if person.birthdate_note:
+                    if len(person.birthdate_note) == 4:
+                        birthyear_as_int = int(person.birthdate_note)
+                        current_year_as_int = int(today.year)
+
+                        if current_year_as_int - birthyear_as_int < 100:
+                            birthdate_info += person.birthdate_note
+
+
+                if person.birthdate:
+                    if today.year - person.birthdate.year < 100:
+                        birthdate_info += str(person.birthdate)
+
+                if birthdate_info:
+                    print(this_display_name + ": " + birthdate_info)
+                    person.living = True
+                    person.save()
