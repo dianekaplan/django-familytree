cd76cadfe03df63a1bea0bb736efca733f7966c5
Diane Kaplan <dianekaplan@gmail.com>
2021-04-23 08:25:15 -0400

    outline view updates

--- mysite/familytree/templatetags/familytree_extras.py
+++ mysite/familytree/templatetags/familytree_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.simple_tag
 def generation_class(generation_int):
     return "g" + str(generation_int)
+
+@register.simple_tag
+def get_class(object):
+    return object.__class__.__name__
