f0f738eb6b48d92bb3c50005d3d32ce5b90b71d2
Diane Kaplan <dianekaplan@gmail.com>
2021-08-20 08:13:19 -0400

    remove unused imports

--- mysite/familytree/models.py
+++ mysite/familytree/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.db.models import Q
-# from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.contrib.admin.models import LogEntry
