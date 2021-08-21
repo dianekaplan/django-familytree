d4d4ad1045ce9ade3c1b9051e744f6e7d6b52a8f
Diane Kaplan <dianekaplan@gmail.com>
2021-08-21 07:43:10 -0400

    feat: added image overlay to say who's pictured (#134)

--- mysite/familytree/models.py
+++ mysite/familytree/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.contrib.admin.models import LogEntry
+from django.utils.safestring import mark_safe
 
 DJANGO_SITE_CREATION = settings.DJANGO_SITE_CREATION
 
@@ -180,6 +181,27 @@ class Image(models.Model):
             image_people.append(person)
 
         return this_image_person, this_image_family, image_people
+
+    def pictured_list(self):
+        results = self.image_subjects()
+        this_image_person = results[0]
+        this_image_family = results[1]
+        image_people = results[2]
+        pictured_list = '<br/>'
+
+        if this_image_person:
+            pictured_list += this_image_person.display_name
+
+        if image_people:  # if the image has both family and individuals configured, show people
+            for person in image_people:
+                pictured_list += person.display_name
+                pictured_list += '<br/>'
+        elif this_image_family:
+            pictured_list += this_image_family.display_name
+
+        pictured_list = mark_safe(pictured_list)
+
+        return pictured_list
 
     class Meta(object):
         verbose_name_plural = 'Images'
