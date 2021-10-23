c5f0223795f77916db846ba85924abe499c3f7a7
Diane Kaplan <dianekaplan@gmail.com>
2021-10-23 16:42:51 -0400

    blah

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # @FIXME: Workaround to populate (meaningless) values when github action runs tests
-if any([arg in sys.argv for arg in ['jenkins', 'test']]):
+if any([arg in sys.argv for arg in ['test']]):
     os.environ['ENV_ROLE'] = 'test'
     os.environ['ROOT_URL'] = 'test'
     os.environ['EMAIL_HOST_PASSWORD'] = 'test'
