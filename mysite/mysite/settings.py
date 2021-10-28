900354d69b8008e877eda8809d3e3e1d30522fc3
Diane Kaplan <dianekaplan@gmail.com>
2021-10-28 07:01:18 -0400

    fix: resolve deprecation warnings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os, sys
 from django.core.exceptions import ImproperlyConfigured
 
+# silence harmless whitenoise issue: https://github.com/evansd/whitenoise/issues/215
+from django.test.utils import ignore_warnings
+ignore_warnings(message="No directory at", module="whitenoise.base").enable()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
