e6efb05d46d3aed401de759ed4b3dc675f4ddb6d
Diane Kaplan <dianekaplan@gmail.com>
2024-10-02 17:41:48 -0400

    Update test file (#265)
    * break out test files
    
    * deleted files

--- mysite/familytree/test_views.py
+++ mysite/familytree/test_views.py
@@ -1,8 +1,14 @@
+import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.mysite.settings")
+
 from django.contrib.auth.models import User
 from django.test import Client, TestCase, override_settings
 from django.urls import resolve, reverse
 
 from .models import Branch, Family, Image, Person, Profile
+
+
 
 
 def create_person(display_name):
@@ -33,67 +39,12 @@ def create_branch(display_name):
     return Branch.objects.create(display_name=display_name)
 
 
-#
-# def create_profile(user):
-#     """
-#     Create a profile for the given user
-#     """
-#     return Profile.objects.create(user=user)
 
-
-class PersonModelTests(TestCase):
-    def setUp(self):
-        self.person = Person()
-        self.person.display_name = "Marcia Brady"
-        self.person.sex = "F"
-        self.person.save()
-
-    def test_fields(self):
-        person = Person()
-        person.display_name = "Jan Brady"
-        person.sex = "F"
-        person.save()
-
-        record = Person.objects.get(pk=person.id)
-        self.assertEqual(record, person)
-
-    def test_default_stories(self):
-        """
-        default person returns None for stories
-        """
-        self.assertIsNone(self.person.has_stories())
-
-    def test_default_families(self):
-        """
-        default person has no families made
-        """
-        self.assertEqual(len(self.person.families_made()), 0)
-
-    def test_default_group_images(self):
-        """
-        default person has no group images
-        """
-        self.assertEqual(len(self.person.group_images()), 0)
-
-    def test_unreviewed_person(self):
-        """
-        unreviewed_person() returns True when reviewed flag is False (default), otherwise False
-        """
-
-        self.assertIs(self.person.unreviewed_person(), True)
-
-        # update reviewed = True, and make sure unreviewed_person() now returns false
-        self.person.reviewed = True
-        self.assertIs(self.person.unreviewed_person(), False)
-
-
-class FamilyModelTests(TestCase):
-    def test_unreviewed_family(self):
-        """
-        unreviewed_family() returns True for family whose reviewed flag is False
-        """
-        unreviewed_family = Family(reviewed=False)
-        self.assertIs(unreviewed_family.unreviewed_family(), True)
+def create_profile(user):
+    """
+    Create a profile for the given user
+    """
+    return Profile.objects.create(user=user)
 
 
 class TestDetailViews(TestCase):
