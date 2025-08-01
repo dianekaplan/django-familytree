5dcf2f35a1eaada654d26e92c6b1bee4c75a9a5c
Diane Kaplan <dianekaplan@gmail.com>
2025-08-01 15:26:30 -0400

    heading change for family history pages, and image detail update

---
+++ mysite/familytree/test_via_browser.py
@@ -0,0 +1,18 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+import time
+
+class Hosttest(LiveServerTestCase): 
+
+    def testhomepage(self): 
+        driver = webdriver.Chrome()
+
+        # driver.get('http://www.google.com/') # works!
+        driver.get('http://127.0.0.1:8000/')  # selenium.common.exceptions.WebDriverException: Message: unknown error: net::ERR_CONNECTION_REFUSED
+        # driver.get('http://127.0.0.1:8000/accounts/login/') # ^ same error as above
+        
+        # driver.get('http://127.0.0.1.8000/familytree') # selenium.common.exceptions.InvalidArgumentException: Message: invalid argument
+        time.sleep(15)
+
+        assert True
+        # assert "Our big" in driver.title
