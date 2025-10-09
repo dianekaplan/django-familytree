0e82628de73ab144fc9af1298ce0841c2fd99e89
Diane Kaplan <dianekaplan@gmail.com>
2025-10-09 09:57:05 -0400

    Dashboard recent updates section: remove and clarify handling for delete change_type, update person text

--- mysite/familytree/views.py
+++ mysite/familytree/views.py
@@ -105,10 +105,9 @@ def index(request):  # dashboard page
         if update.content_type_id == 5:  # Story update (including association with person/family)
             updated_story = Story.objects.get(id=update.object_id)
 
-        content_type = str(ContentType.objects.get(id=update.content_type_id)).replace("familytree | ", "")
-        change_type = "added" if update.action_flag == 1 else "updated"
-        if update.action_flag == 3:
-            change_type = "deleted"
+        content_type = str(ContentType.objects.get(id=update.content_type_id)).replace("Familytree | ", "")
+        change_type = "added" if update.action_flag == 1 else "updated"  # (update corresponds to 2)
+        # 3 is for deletion (where we won't have an updated object to link to) so we don't show that as a change_type
         combination = [update, user_person, content_type, change_type, updated_person, updated_story]
         recent_updates.append(combination)
 
